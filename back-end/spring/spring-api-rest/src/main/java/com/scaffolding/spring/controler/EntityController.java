0ac56ec4e69fbff15eae3e53c0afd4ec707184c5
jacastellano <jacastellanoa@gmail.com>
2019-02-12 15:49:13 +0100

    Adding Unit Testing Controller & Improving controller

--- back-end/spring/spring-api-rest/src/main/java/com/scaffolding/spring/controler/EntityController.java
+++ back-end/spring/spring-api-rest/src/main/java/com/scaffolding/spring/controler/EntityController.java
@@ -1,8 +1,11 @@
 package com.scaffolding.spring.controler;
 
 import java.util.Date;
-import java.util.List;
 
+import javax.inject.Inject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,40 +14,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.scaffolding.spring.error.EntityNotFoundException;
 import com.scaffolding.spring.model.EntityModel;
 import com.scaffolding.spring.persistence.repository.EntityRepository;
 
 @RestController
 public class EntityController {
 
-	private final EntityRepository repository;
-
-	public EntityController(EntityRepository repository) {
-		this.repository = repository;
-	}
-
-	/**
-	 * Get all entities
-	 * 
-	 * @return a complete list of entities
-	 */
-	@GetMapping("/entities")
-	public List<EntityModel> all() {
-		return repository.findAll();
-	}
-
-	/**
-	 * Create a new entity
-	 * 
-	 * @param newEntity
-	 * @return the entity object that it was created
-	 */
-	@PostMapping("/entities")
-	public EntityModel newEntity(@RequestBody EntityModel newEntity) {
-		newEntity.setCreateDate(new Date());
-		return repository.save(newEntity);
-	}
+	@Inject
+	private EntityRepository repository;
 
 	/**
 	 * Get a specific entity
@@ -53,30 +30,50 @@ public class EntityController {
 	 * @return the entity object that it was found
 	 */
 	@GetMapping("/entities/{entityId}")
-	public EntityModel one(@PathVariable Long entityId) {
-		return repository.findById(entityId).orElseThrow(() -> new EntityNotFoundException(entityId));
+	public ResponseEntity<EntityModel> findEntityById(@PathVariable Long entityId) {
+
+		return repository.findById(entityId).map(entity -> new ResponseEntity<>(entity, HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+
+	/**
+	 * Get all entities
+	 * 
+	 * @return a complete list of entities
+	 */
+	@GetMapping("/entities")
+	public ResponseEntity<Iterable<EntityModel>> findAllEntities() {
+		return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+	}
+
+	/**
+	 * Create a new entity
+	 * 
+	 * @param entity
+	 * @return the entity object that it was created
+	 */
+	@PostMapping("/entities")
+	public ResponseEntity<EntityModel> createEntity(@RequestBody EntityModel entity) {
+		entity.setCreateDate(new Date());
+		return new ResponseEntity<>(repository.save(entity), HttpStatus.CREATED);
 	}
 
 	/**
 	 * Update an entity
 	 * 
-	 * @param newEntity an entity object with new values
+	 * @param entity an entity object with new values
 	 * @param entityId
 	 * @return the entity object that it was updated
 	 */
 	@PutMapping("/entities/{entityId}")
-	public EntityModel replaceEntity(@RequestBody EntityModel newEntity, @PathVariable Long entityId) {
+	public ResponseEntity<EntityModel> updateEntity(@RequestBody EntityModel entity, @PathVariable Long entityId) {
 
-		return repository.findById(entityId).map(entity -> {
-			entity.setEntityTitle(newEntity.getEntityTitle());
-			entity.setEntityDescription(newEntity.getEntityDescription());
-			entity.setUpdateDate(new Date());
-			return repository.save(entity);
-		}).orElseGet(() -> {
-			newEntity.setEntityId(entityId);
-			newEntity.setCreateDate(new Date());
-			return repository.save(newEntity);
-		});
+		return repository.findById(entityId).map(e -> {
+			e.setEntityTitle(entity.getEntityTitle());
+			e.setEntityDescription(entity.getEntityDescription());
+			e.setUpdateDate(new Date());
+			return new ResponseEntity<>(repository.save(e), HttpStatus.OK);
+		}).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 
 	/**
@@ -85,8 +82,12 @@ public class EntityController {
 	 * @param entityId
 	 */
 	@DeleteMapping("/entities/{entityId}")
-	public void deleteEntity(@PathVariable Long entityId) {
-		repository.deleteById(entityId);
+	public ResponseEntity<EntityModel> deleteEntity(@PathVariable Long entityId) {
+
+		return repository.findById(entityId).map(e -> {
+			e.setDeleteDate(new Date());
+			return new ResponseEntity<>(repository.save(e), HttpStatus.OK);
+		}).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 
 }
--- back-end/spring/spring-api-rest/src/test/java/com/scaffolding/spring/SpringApiRestApplicationTests.java
+++ back-end/spring/spring-api-rest/src/test/java/com/scaffolding/spring/SpringApiRestApplicationTests.java
@@ -1,16 +1,208 @@
 package com.scaffolding.spring;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.scaffolding.spring.model.EntityModel;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringApiRestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringApiRestApplicationTests {
+
+	@Inject
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
+	}
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testFindEntityById_Found() {
+
+		// Request
+		ResponseEntity<EntityModel> response = restTemplate.getForEntity(getRootUrl() + "/entities/1",
+				EntityModel.class);
+
+		// Assertions
+		Assert.assertNotNull(response);
+		Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+	}
+
+	@Test
+	public void testFindEntityById_NotFound() {
+
+		// Request
+		ResponseEntity<EntityModel> response = restTemplate.getForEntity(getRootUrl() + "/entities/1000000",
+				EntityModel.class);
+
+		// Assertions
+		Assert.assertNotNull(response);
+		Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+	}
+
+	@Test
+	public void testFindAllEntities() {
+
+		// Prepare request
+		ParameterizedTypeReference<List<EntityModel>> responseType = new ParameterizedTypeReference<List<EntityModel>>() {
+		};
+
+		// Request
+		ResponseEntity<List<EntityModel>> response = restTemplate.exchange(getRootUrl() + "/entities", HttpMethod.GET,
+				null, responseType);
+
+		// Assertions
+		Assert.assertNotNull(response.getBody());
+		Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+	}
+
+	@Test
+	public void testCreateEntity() {
+
+		// Prepare mock data
+		EntityModel mockEntity = new EntityModel();
+		mockEntity.setEntityTitle("Entity for testing");
+		mockEntity.setEntityDescription("Creating an entity for testing...");
+
+		// Prepare request
+		ResponseEntity<EntityModel> response = restTemplate.postForEntity(getRootUrl() + "/entities", mockEntity,
+				EntityModel.class);
+
+		// Response
+		EntityModel createdEntity = response.getBody();
+
+		// Assertions
+		Assert.assertNotNull(response);
+		Assert.assertNotNull(response.getBody());
+		Assert.assertEquals(mockEntity.getEntityTitle(), createdEntity.getEntityTitle());
+		Assert.assertEquals(mockEntity.getEntityDescription(), createdEntity.getEntityDescription());
+		Assert.assertNotNull(createdEntity.getEntityId());
+		Assert.assertNotNull(createdEntity.getCreateDate());
+		Assert.assertNull(createdEntity.getUpdateDate());
+		Assert.assertNull(createdEntity.getDeleteDate());
+		Assert.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+	}
+
+	@Test
+	public void testUpdateEntity_Found() throws JsonProcessingException {
+
+		// Prepare mock data
+		EntityModel mockEntity = new EntityModel();
+		mockEntity.setEntityTitle("Entity for testing");
+		mockEntity.setEntityDescription("Updating an entity for testing...");
+
+		// Prepare request
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String requestBody = ow.writeValueAsString(mockEntity);
+		HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody, headers);
+
+		// Request
+		ResponseEntity<EntityModel> response = restTemplate.exchange(getRootUrl() + "/entities/2", HttpMethod.PUT,
+				httpEntity, EntityModel.class);
+
+		// Response
+		EntityModel updatedEntity = response.getBody();
+
+		// Assertions
+		Assert.assertNotNull(response);
+		Assert.assertNotNull(response.getBody());
+		Assert.assertEquals("Entity for testing", updatedEntity.getEntityTitle());
+		Assert.assertEquals("Updating an entity for testing...", updatedEntity.getEntityDescription());
+		Assert.assertNotNull(updatedEntity.getEntityId());
+		Assert.assertNotNull(updatedEntity.getCreateDate());
+		Assert.assertNotNull(updatedEntity.getUpdateDate());
+		Assert.assertNull(updatedEntity.getDeleteDate());
+		Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+	}
+
+	@Test
+	public void testUpdateEntity_NotFound() throws JsonProcessingException {
+
+		// Prepare mock data
+		EntityModel mockEntity = new EntityModel();
+		mockEntity.setEntityTitle("Entity for testing");
+		mockEntity.setEntityDescription("Updating an entity for testing...");
+
+		// Prepare request
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String requestBody = ow.writeValueAsString(mockEntity);
+		HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody, headers);
+
+		// Request
+		ResponseEntity<EntityModel> response = restTemplate.exchange(getRootUrl() + "/entities/1000000", HttpMethod.PUT,
+				httpEntity, EntityModel.class);
+
+		// Assertions
+		Assert.assertNotNull(response);
+		Assert.assertNull(response.getBody());
+		Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+	}
+
+	@Test
+	public void testDeleteEntity_Found() {
+
+		// Request
+		ResponseEntity<EntityModel> response = restTemplate.exchange(getRootUrl() + "/entities/3", HttpMethod.DELETE,
+				null, EntityModel.class);
+
+		// Response
+		EntityModel deletedEntity = response.getBody();
+
+		// Assertions
+		Assert.assertNotNull(response);
+		Assert.assertNotNull(response.getBody());
+		Assert.assertNotNull(deletedEntity.getDeleteDate());
+		Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+	}
+
+	@Test
+	public void testDeleteEntity_NotFound() {
+
+		// Request
+		ResponseEntity<EntityModel> response = restTemplate.exchange(getRootUrl() + "/entities/1000000",
+				HttpMethod.DELETE, null, EntityModel.class);
+
+		// Assertions
+		Assert.assertNotNull(response);
+		Assert.assertNull(response.getBody());
+		Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
 	}
 
 }
