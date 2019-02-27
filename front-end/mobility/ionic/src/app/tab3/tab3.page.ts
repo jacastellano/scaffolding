f544590bcce80808e00ab8e0495aead666af3431
jacastellano <jacastellanoa@gmail.com>
2019-02-27 17:13:14 +0100

    Adding geolocation support

--- front-end/mobility/ionic/src/app/tab3/tab3.page.ts
+++ front-end/mobility/ionic/src/app/tab3/tab3.page.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+interface Position {
+  latitude: number;
+  longitude: number;
+}
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit {
+
+  public currentPosition: Position = {
+    latitude: 0,
+    longitude: 0
+  };
+
+  constructor(private geolocation: Geolocation) { }
+
+  ngOnInit(): void {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.currentPosition = {
+        latitude: resp.coords.latitude,
+        longitude: resp.coords.longitude
+      };
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+}
