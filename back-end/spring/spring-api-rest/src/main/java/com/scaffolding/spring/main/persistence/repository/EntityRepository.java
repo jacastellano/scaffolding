41f09d6854d6a4188fe2c96c9590326fc7569b05
jacastellano <jacastellanoa@gmail.com>
2019-03-06 19:12:32 +0100

    Improving api rest: exclude deleted

--- back-end/spring/spring-api-rest/src/main/java/com/scaffolding/spring/main/persistence/repository/EntityRepository.java
+++ back-end/spring/spring-api-rest/src/main/java/com/scaffolding/spring/main/persistence/repository/EntityRepository.java
@@ -1,9 +1,19 @@
 package com.scaffolding.spring.main.persistence.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.scaffolding.spring.main.persistence.domain.EntityDAO;
 
 public interface EntityRepository extends JpaRepository<EntityDAO, Long> {
+
+	@Query("SELECT e FROM EntityDAO e WHERE e.deleteDate IS NULL")
+	List<EntityDAO> findAll();
+	
+	@Query("SELECT e FROM EntityDAO e WHERE e.entityId =?1 AND e.deleteDate IS NULL")
+	Optional<EntityDAO> findById(Long id);
 
 }
