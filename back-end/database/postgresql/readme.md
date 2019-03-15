f8c321e9de38eb742a3aaa71e092055c8493920c
jacastellano <jacastellanoa@gmail.com>
2019-03-15 08:46:02 +0100

    Authorization Server (First Version)

--- back-end/database/postgresql/readme.md
+++ back-end/database/postgresql/readme.md
@@ -17,24 +17,8 @@
     CREATE SCHEMA comun;
     CREATE SCHEMA entity;
     ```
-* Create Entity table
+* Create Entity table:
+	Execute entity.sql
 
-    ```
-    CREATE TABLE entity.entity
-	(
-		id bigint NOT NULL DEFAULT nextval('entity.entity_id_seq'::regclass),
-		create_date timestamp without time zone,
-		delete_date timestamp without time zone,
-		description character varying(255) COLLATE pg_catalog."default",
-		title character varying(255) COLLATE pg_catalog."default",
-		update_date timestamp without time zone,
-		CONSTRAINT entity_pkey PRIMARY KEY (id)
-	)
-	WITH (
-		OIDS = FALSE
-	)
-	TABLESPACE pg_default;
-
-	ALTER TABLE entity.entity
-		OWNER to postgres;
-    ```
+* Create Spring Oauth2 Data Model:
+	Execute oauth.sql
---
+++ back-end/spring/authorization-server/src/main/java/com/scaffolding/spring/security/config/AuthorizationServerConfig.java
@@ -0,0 +1,58 @@
+package com.scaffolding.spring.security.config;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Inject
+	private Environment environment;
+
+	@Inject
+	@Qualifier("authenticationManagerBean")
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.jdbc(dataSource());
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+	}
+
+	public TokenStore tokenStore() {
+		return new JdbcTokenStore(dataSource());
+	}
+
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+		dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+		dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+		return dataSource;
+	}
+
+}
